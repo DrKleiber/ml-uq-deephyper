546c15f2251ff2cbf827c3ab269ade51aeaaa3db
DrKleiber <wellliu127@gmail.com>
2022-07-19 18:40:02 -0500

    Update DataLoader_2D.py

--- utils/DataLoader_2D.py
+++ utils/DataLoader_2D.py
@@ -8,7 +8,7 @@ Created on Wed Jan  8 15:22:45 2020
 
 import torch
 from torch.utils.data import Dataset
-        
+
 class StarDataset(Dataset):
     def __init__(self, input_list, output_list):
         'initilization only, not loading data'
@@ -16,7 +16,7 @@ class StarDataset(Dataset):
         self.output_list = output_list
         if len(self.input_list) != len(self.output_list) :
             raise ValueError('length of input list and output list must be the same!')
-            
+
     def __len__(self):
         'Denotes the total number of samples'
         return len(self.input_list)
@@ -26,16 +26,16 @@ class StarDataset(Dataset):
         if self.input_list[index][-7:-3] != self.output_list[index][-7:-3]:
             print(self.input_list[index])
             print(self.output_list[index])
-            raise ValueError('input and output pair are inconsistent!') 
-        
+            raise ValueError('input and output pair are inconsistent!')
+
         # load data
         input_features = torch.load(self.input_list[index])
         A_idx = [0,2,3,4]  # remove velocity j component from inputs
-        input_features = input_features[A_idx,int(output_qois.size()[1]/2),:,:]   # cut one slice from the data
-        
+        input_features = input_features[A_idx,16,:,:]   # cut one slice from the data
+
         output_qois = torch.load(self.output_list[index])
-        output_qois = output_qois[0,int(output_qois.size()[1]/2),:,:].unsqueeze(0)
-        
+        output_qois = output_qois[0,16,:,:].unsqueeze(0)
+
         # convert to float type
         input_features = input_features.type(torch.float)
         output_qois = output_qois.type(torch.float)
